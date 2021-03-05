74f4fb1233b52f333f8d96a7d2e9281b470fc575
Md Siamul Islam <siamul.islam@northsouth.edu>
2021-03-05 17:42:29 +0600

    component creating

--- src/App.js
+++ src/App.js
@@ -2,35 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  var name = {
-    name : "Md Siamul Isalm",
-    profession:"React Developer",
-    adress : "11,shahjalal road ,konapara,demra,dhaka"
-  }
-  var myStyle={
-    color: "red",
-    backgroundColor:'yellow'
-  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
-        <h1 style={myStyle}>My Heading : {name.adress}</h1>
-        <p style={{color:'black', backgroundColor:'white'}}>My first React Project</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Click Here
-        </a>
+        <p >SIAM</p>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
   );
+}
+
+function Person(){
+  return (
+  <div style={{border:'2px solid red',padding : '2px',margin:'12px'}}>
+    <h1>Name : Shakib Al Hasan</h1>
+    <h2>He is a very good cricketer</h2>
+  </div>
+  )
 }
 
 export default App;
